9d740abb053fb8ae675fbef068f159fb0a86e306
Eric Fan <yfanal@connect.ust.hk>
2021-09-06 15:51:07 +0800

    add assets

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import * as ethereumConfig from './config/ethereum.json';
+import * as kovanConfig from './config/kovan.json';
+import * as polygonConfig from './config/polygon.json';
+import * as bscConfig from './config/bsc.json';
+import * as arbitrumConfig from './config/arbitrum.json';
+
+
+import * as ethereumAsset from './asset/ethereum.json';
+import * as kovanAsset from './asset/kovan.json';
+import * as polygonAsset from './asset/polygon.json';
+import * as bscAsset from './asset/bsc.json';
+import * as arbitrumAsset from './asset/arbitrum.json';
+
+
+import ERC20_ABI from './abi/basic/ERC20.json';
+import GLOBAL_CONFIG_ABI from './abi/basic/GlobalConfig.json';
+import AMM_ABI from './abi/basic/Amm.json';
+import FACTORY_ABI from './abi/basic/Factory.json';
+import READER_ABI from './abi/basic/Reader.json';
+import FUTURES_ABI from './abi/basic/Futures.json';
+import KOVAN_ERC20_ABI from './abi/basic/Kovan_ERC20.json';
+
+
+export {
+    ethereumConfig,
+    kovanConfig,
+    polygonConfig,
+    bscConfig,
+    arbitrumConfig,
+
+    ethereumAsset,
+    kovanAsset,
+    polygonAsset,
+    bscAsset,
+    arbitrumAsset,
+
+    ERC20_ABI, 
+    GLOBAL_CONFIG_ABI, 
+    AMM_ABI, 
+    FACTORY_ABI, 
+    READER_ABI, 
+    FUTURES_ABI, 
+    KOVAN_ERC20_ABI    
+
+  };
