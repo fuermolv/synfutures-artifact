9cd153f6329ea63e6d95e91d0876496b61a41501
Eric Fan <yfanal@connect.ust.hk>
2021-09-06 15:59:18 +0800

    remove index.ts

--- src/abi/index.ts
+++
@@ -1,9 +0,0 @@
-import ERC20_ABI from './ERC20.json';
-import GLOBAL_CONFIG_ABI from './GlobalConfig.json';
-import AMM_ABI from './Amm.json';
-import FACTORY_ABI from './Factory.json';
-import READER_ABI from './Reader.json';
-import FUTURES_ABI from './Futures.json';
-import KOVAN_ERC20_ABI from './Kovan_ERC20.json';
-
-export { ERC20_ABI, GLOBAL_CONFIG_ABI, AMM_ABI, FACTORY_ABI, READER_ABI, FUTURES_ABI, KOVAN_ERC20_ABI };
