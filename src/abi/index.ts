a427dceab45a6896a6292994fae16a090358f30f
jxiaox <jxiaox@hotmail.com>
2021-09-22 18:21:22 +0800

    move files to simplify the main file

---
+++ src/abi/index.ts
@@ -0,0 +1,19 @@
+import * as ERC20_ABI from './ERC20.json';
+import * as GLOBAL_CONFIG_ABI from './GlobalConfig.json';
+import * as AMM_ABI from './Amm.json';
+import * as FACTORY_ABI from './Factory.json';
+import * as READER_ABI from './Reader.json';
+import * as FUTURES_ABI from './Futures.json';
+import * as KOVAN_ERC20_ABI from './Kovan_ERC20.json';
+import * as BTCHASH_ORACLE_ABI from './oracle/bitcoin-mining-tracker.json';
+
+export {
+  ERC20_ABI,
+  GLOBAL_CONFIG_ABI,
+  AMM_ABI,
+  FACTORY_ABI,
+  READER_ABI,
+  FUTURES_ABI,
+  KOVAN_ERC20_ABI,
+  BTCHASH_ORACLE_ABI,
+};
---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import * as ethereumConfig from './ethereum.json';
+import * as kovanConfig from './kovan.json';
+import * as polygonConfig from './polygon.json';
+import * as bscConfig from './bsc.json';
+import * as arbitrumConfig from './arbitrum.json';
+
+export { ethereumConfig, kovanConfig, polygonConfig, bscConfig, arbitrumConfig };
