641de51eb146a408cf6e66311a95fc6b2ac0313f
jxiaox <jxiaox@hotmail.com>
2021-09-29 17:15:09 +0800

    add btc hashrate abi

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -5,8 +5,13 @@ import * as FACTORY_ABI from './Factory.json';
 import * as READER_ABI from './Reader.json';
 import * as FUTURES_ABI from './Futures.json';
 import * as KOVAN_ERC20_ABI from './Kovan_ERC20.json';
-import * as BTCHASH_MINING_TRACKER_ABI from './oracle/bitcoin-mining-tracker.json';
-import * as BTCHASH_ORACLE_ABI from './btchash/Oracle.json';
+import * as BTCHASHRATE_MINING_TRACKER_ABI from './oracle/bitcoin-mining-tracker.json';
+import * as BTCHASHRATE_ORACLE_ABI from './BtcHashRate/Oracle.json';
+import * as BTCHASHRATE_GLOBAL_CONFIG_ABI from './BtcHashRate/GlobalConfig.json';
+import * as BTCHASHRATE_AMM_ABI from './BtcHashRate/Amm.json';
+import * as BTCHASHRATE_FACTORY_ABI from './BtcHashRate/Factory.json';
+import * as BTCHASHRATE_READER_ABI from './BtcHashRate/Reader.json';
+import * as BTCHASHRATE_FUTURES_ABI from './BtcHashRate/Futures.json';
 
 export {
   ERC20_ABI,
@@ -16,6 +21,11 @@ export {
   READER_ABI,
   FUTURES_ABI,
   KOVAN_ERC20_ABI,
-  BTCHASH_ORACLE_ABI,
-  BTCHASH_MINING_TRACKER_ABI,
+  BTCHASHRATE_MINING_TRACKER_ABI,
+  BTCHASHRATE_ORACLE_ABI,
+  BTCHASHRATE_GLOBAL_CONFIG_ABI,
+  BTCHASHRATE_AMM_ABI,
+  BTCHASHRATE_FACTORY_ABI,
+  BTCHASHRATE_READER_ABI,
+  BTCHASHRATE_FUTURES_ABI,
 };
